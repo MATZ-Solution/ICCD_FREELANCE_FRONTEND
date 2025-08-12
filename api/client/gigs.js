b685b35e34c81e18097c02378d7df48fa5311f59
UmairNazakat <131239045+UmairNazakat@users.noreply.github.com>
2025-08-12 12:43:12 -0700

    Update gigs.js

--- api/client/gigs.js
+++ api/client/gigs.js
@@ -1,4 +1,4 @@
-// import API_ROUTE from "../endpoints";
+import API_ROUTE from "../endPoints";
 import { useMutation } from "@tanstack/react-query";
 import api from "../axios/index";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
