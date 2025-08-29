b7286c6bf99acb68266c32051c574efddddbcb75
sohaibnayyar721 <sohaibnayyar.721@gmail.com>
2025-08-29 18:32:12 +0500

    changes in ui

--- src/component/super_admin/DisputeDetailView.jsx
+++ src/component/super_admin/DisputeDetailView.jsx
@@ -14,10 +14,12 @@ import { useGetDisputeAdminById } from "../../../api/client/dispute";
 
 export default function ViewDisputeDetail() {
   const { id } = useParams();
-  const { data, isSuccess, isLoading, isError } = useGetDisputeAdminById(id);
+  const { data,responseData, isSuccess, isLoading, isError } = useGetDisputeAdminById(id);
 
   const [partialAmount, setPartialAmount] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
+
+  console.log("responseData: ", responseData)
 
   if (isLoading) return <div className="p-6 text-center">Loading...</div>;
   if (isError)
